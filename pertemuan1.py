5a0f4b80cf5c09080bfe310b6b854daad4726db3
andra343 <setiawanananda32@gmail.com>
2026-01-26 18:23:22 +0700

    Senin-26-januari-2026

---
+++ pertemuan1.py
@@ -0,0 +1,31 @@
+print("hello")
+
+print("hai ini atas " \
+      "ini bawah")
+
+print("coba comment") #ini komentar
+
+nama        = "Indra"   #ini str (string / kata)
+umur        = 13        #ini int (integer / bilangan bulat)
+tinggi      = 1.73      #ini float (bilangan desimal)
+lulus       = True      #ini bool (boolean / benar atau salah)
+hobi        = ["eating", "sleeping", "coding", "mancing"] #ini list
+
+#panggil variabel
+print("hallo nama saya", nama , "umur saya", umur, "tinggi saya", tinggi, "hobi saya", hobi)
+
+#cara tau tipe datanya
+print(type(nama))
+print(type(umur))
+print(type(tinggi))
+print(type(lulus))
+print(type(hobi))
+
+ucapan = input("Kasih ucapan buat KING MU = ")
+print(ucapan)
+
+nama        = input("Masukan nama anda: ")
+usia        = int(input("masukan usia anda: "))
+tinggi      = float(input("masukan tinggi badan anda: "))
+
+print("hallo", nama,"umur anda", usia, "tinggi badan anda", tinggi)
